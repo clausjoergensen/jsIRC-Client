000b6967f2f273cd40e18ae2501022daad402c2b
Claus Joergensen <claus.joergensen@outlook.com>
2018-08-15 15:03:59 +0200

    Added input history (globally atm.)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 'use strict'
 
 const IrcViewController = require('./IrcViewController.js')
+const inputhistory = require('./inputhistory.js')
+const $ = require('jquery')
 
 let viewController = null // eslint-disable-line no-unused-vars
 let client = null // eslint-disable-line no-unused-vars
 
 document.addEventListener('DOMContentLoaded', function (event) {
   viewController = new IrcViewController()
+  inputhistory($("#chat-input"))
 })
---
+++ src/inputhistory.js
@@ -0,0 +1,42 @@
+/*!
+* Modified version of https://github.com/erming/inputhistory for Node.js
+*/
+module.exports = function (input) {
+  let options = {
+    history: [],
+    preventSubmit: false
+  }
+  let history = options.history
+  history.push('')
+  let i = 0
+  input.on('keydown', function (e) {
+    let key = e.which
+    switch (key) {
+      case 13: // Enter
+        if (input.val() !== '') {
+          i = history.length
+          history[i - 1] = input.val()
+          history.push('')
+          if (history[i - 1] === history[i - 2]) {
+            history.splice(-2, 1)
+            i--
+          }
+        }
+        break
+      case 38: // Up
+      case 40: // Down
+        history[i] = input.val()
+        if (key === 38 && i !== 0) {
+          i--
+        } else if (key === 40 && i < history.length - 1) {
+          i++
+        }
+        input.val(history[i])
+        break
+      default:
+        return
+    }
+    e.preventDefault()
+  })
+  return input
+}
