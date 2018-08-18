fb929164512d82284d142798b9fa90ef17081296
Claus Joergensen <claus.joergensen@outlook.com>
2018-08-18 23:23:21 +0200

    Removed unused variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 const IrcViewController = require('./IrcViewController.js')
 
 let viewController = null // eslint-disable-line no-unused-vars
-let client = null // eslint-disable-line no-unused-vars
 
 document.addEventListener('DOMContentLoaded', function (event) {
   viewController = new IrcViewController()
